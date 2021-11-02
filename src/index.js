9e71bc5a7c6a1ee398a94de9c3b62183dbe3a29f
v.shustov <57774085+shuimi@users.noreply.github.com>
2021-11-03 01:32:55 +0300

    -interpolation added

--- src/index.js
+++ src/index.js
@@ -9,18 +9,57 @@ document.addEventListener('DOMContentLoaded', function () {
     const sourceFunctionInput = document.getElementById('function');
     const leftBoundInput = document.getElementById('function_left_bound');
     const rightBoundInput = document.getElementById('function_right_bound');
+    const stepInput = document.getElementById('step');
+    const functionSplit = document.getElementById('function_split');
 
     let sourceFunction = (x) => x;
-    let leftBound = -10.;
-    let rightBound = 10.;
+    let leftBound = -5.;
+    let rightBound = 5.;
+    let step = 1;
+
+    const interpolation = (x, y) => {
+
+        if (x.length !== y.length) {
+            throw new Error('Invalid x, y size.');
+        }
+
+        const n = x.length;
+
+        const subpoly = (t, i) => {
+            let multiply = 1.;
+
+            for (let j = 0; j < n; j++) {
+                if (i !== j) {
+                    multiply *= (t - x[j]) / (x[i] - x[j]);
+                }
+            }
+            return multiply;
+        }
+
+        return (t) => {
+            let summation = 0;
+            for (let i = 0; i < n; i++) {
+                summation += y[i] * subpoly(t, i);
+            }
+            return summation;
+        }
+
+    }
 
     const plot = () => {
 
-        let x = _.range(leftBound, rightBound + 1);
+        let x = _.range(leftBound, rightBound + 1, step);
         let y = x.map(value => sourceFunction(value));
 
+        functionSplit.value = y;
+
+        const interpolate = interpolation(x, y);
+
+        let xpoly = _.range(leftBound, rightBound + 1, 0.1);
+        let poly = xpoly.map(value => interpolate(value));
+
         Plotly.newPlot('chart', {
-            data: [ { x: x, y: y } ],
+            data: [ { x: x, y: y }, { x: xpoly, y: poly } ],
             layout: {
                 width: 600,
                 height: 400
@@ -43,14 +82,21 @@ document.addEventListener('DOMContentLoaded', function () {
         rightBound = Number(rightBoundInput.value);
     }
 
+    const onStepChange = () => {
+        step = Number(stepInput.value);
+    }
+
     sourceFunctionInput.addEventListener('input', onInputChange);
     leftBoundInput.addEventListener('input', onLeftBoundChange);
     rightBoundInput.addEventListener('input', onRightBoundChange);
+    stepInput.addEventListener('input', onStepChange);
 
     sourceFunctionInput.addEventListener('blur', plot);
     leftBoundInput.addEventListener('blur', plot);
     rightBoundInput.addEventListener('blur', plot);
+    stepInput.addEventListener('blur', plot);
 
+    plot();
 });
 
 // accuracy
